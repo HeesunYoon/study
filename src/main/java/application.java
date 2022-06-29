646d7b53b4a7f84481bb74dca3b8600a05497749
spge-dev <grapewater@daum.net>
2022-06-29 14:58:20 +0900

    클래스 추가

---
+++ src/main/java/application.java
@@ -0,0 +1,10 @@
+import study.MathRound;
+
+public class application {
+
+    public static void main(String[] args) {
+        MathRound mathRound = new MathRound();
+        System.out.println(mathRound.num);
+
+    }
+}
---
+++ src/main/java/study/MathRound.java
@@ -0,0 +1,9 @@
+package study;
+
+public class MathRound {
+    public int num = 11;
+
+    public void MathRound(){
+
+    }
+}
